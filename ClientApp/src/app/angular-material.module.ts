10066ed1e36575bf0ee1593f24697c2220f5205e
Roman <Roman@ROMAN-7-PRO>
2021-01-05 09:49:17 -0500

    working paginator

--- ClientApp/src/app/angular-material.module.ts
+++ ClientApp/src/app/angular-material.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @NgModule({
-  imports: [MatTableModule],
-  exports: [MatTableModule]
+  imports: [MatTableModule, MatPaginatorModule],
+  exports: [MatTableModule, MatPaginatorModule]
 })
 
 export class AngularMaterialModule { }
--- ClientApp/src/app/cities/cities.component.ts
+++ ClientApp/src/app/cities/cities.component.ts
@@ -1,5 +1,7 @@
-import { Component, Inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, Inject, ViewChild } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { City } from './city';
 
 
@@ -11,17 +13,36 @@ import { City } from './city';
 
 export class CitiesComponent {
   public displayedColumns: string[] = ['id', 'name', 'lat', 'lon'];
-  public cities: City[];
+  public cities: MatTableDataSource<City>;
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
   constructor(
     private http: HttpClient,
     @Inject('BASE_URL') private baseURL: string) { }
 
   ngOnInit() {
-    this.http.get<City[]>(this.baseURL + 'api/Cities')
+    var pageEvent = new PageEvent();
+    pageEvent.pageIndex = 0;
+    pageEvent.pageSize=10;
+    this.getData(pageEvent);
+  }
+
+  getData(event: PageEvent) {
+    var url = this.baseURL + 'api/Cities';
+
+    var params = new HttpParams()
+      .set("pageIndex", event.pageIndex.toString())
+      .set("pageSize", event.pageSize.toString());
+
+    this.http.get<any>(url, { params })
       .subscribe(result => {
-        this.cities = result;
+        this.paginator.length = result.totalCount;
+        this.paginator.pageIndex = result.pageIndex;
+        this.paginator.pageSize = result.pageSize;
+        this.cities = new MatTableDataSource<City>(result.data);
       }, error => console.error(error));
+
   }
 
 }
