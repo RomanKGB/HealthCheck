999e4c7eb665894183f2e86184776e040db8421a
Roman <Roman@ROMAN-7-PRO>
2021-01-05 17:10:18 -0500

    Working cities with sort and filtering

--- ClientApp/src/app/angular-material.module.ts
+++ ClientApp/src/app/angular-material.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
-  imports: [MatTableModule, MatPaginatorModule],
-  exports: [MatTableModule, MatPaginatorModule]
+  imports: [MatTableModule, MatPaginatorModule, MatSortModule, MatInputModule],
+  exports: [MatTableModule, MatPaginatorModule, MatSortModule, MatInputModule]
 })
 
 export class AngularMaterialModule { }
--- ClientApp/src/app/cities/cities.component.ts
+++ ClientApp/src/app/cities/cities.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject, ViewChild } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 import { City } from './city';
 
 
@@ -14,17 +15,32 @@ import { City } from './city';
 export class CitiesComponent {
   public displayedColumns: string[] = ['id', 'name', 'lat', 'lon'];
   public cities: MatTableDataSource<City>;
+  defaultPageIndex: number = 0;
+  defaultPageSize: number = 10;
+  public defaultSortColumn: string = "name";
+  public defaultSortOrder: string = "asc";
+  defaultFilterColumn: string = "name";
+  filterQuery: string = null;
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
   constructor(
     private http: HttpClient,
     @Inject('BASE_URL') private baseURL: string) { }
 
   ngOnInit() {
+    this.loadData(null);
+  }
+
+  loadData(query:string=null) {
     var pageEvent = new PageEvent();
-    pageEvent.pageIndex = 0;
-    pageEvent.pageSize=10;
+    pageEvent.pageIndex = this.defaultPageIndex;
+    pageEvent.pageSize = this.defaultPageSize;
+    if (query) {
+      this.filterQuery = query;
+    }
+    console.log(query);
     this.getData(pageEvent);
   }
 
@@ -33,7 +49,18 @@ export class CitiesComponent {
 
     var params = new HttpParams()
       .set("pageIndex", event.pageIndex.toString())
-      .set("pageSize", event.pageSize.toString());
+      .set("pageSize", event.pageSize.toString())
+      .set("sortColumn", (this.sort)
+        ? this.sort.active
+        : this.defaultSortColumn)
+      .set("sortOrder", (this.sort)
+        ? this.sort.direction
+        : this.defaultSortOrder);
+
+    if (this.filterQuery) {
+      params = params.set("filterColumn", this.defaultFilterColumn)
+        .set("filterQuery", this.filterQuery);
+    }
 
     this.http.get<any>(url, { params })
       .subscribe(result => {
@@ -41,8 +68,9 @@ export class CitiesComponent {
         this.paginator.pageIndex = result.pageIndex;
         this.paginator.pageSize = result.pageSize;
         this.cities = new MatTableDataSource<City>(result.data);
+        console.log(result.data);
       }, error => console.error(error));
-
+    
   }
 
 }
