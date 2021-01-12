532128d74b4c61361f9b9d113bc8911098248733
Roman <Roman@ROMAN-7-PRO>
2021-01-12 16:59:24 -0500

    added material select

--- ClientApp/src/app/angular-material.module.ts
+++ ClientApp/src/app/angular-material.module.ts
@@ -3,10 +3,11 @@ import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
-  imports: [MatTableModule, MatPaginatorModule, MatSortModule, MatInputModule],
-  exports: [MatTableModule, MatPaginatorModule, MatSortModule, MatInputModule]
+  imports: [MatTableModule, MatPaginatorModule, MatSortModule, MatInputModule, MatSelectModule],
+  exports: [MatTableModule, MatPaginatorModule, MatSortModule, MatInputModule, MatSelectModule]
 })
 
 export class AngularMaterialModule { }
