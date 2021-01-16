46aa63bd284a78e8d4941b4d42e1d47e7ab9b353
Roman <Roman@ROMAN-7-PRO>
2021-01-16 15:12:41 -0500

    added a CountryDTO class

--- ClientApp/src/app/countries/countries.component.ts
+++ ClientApp/src/app/countries/countries.component.ts
@@ -12,7 +12,7 @@ import { Country } from './country';
 })
 
 export class CountriesComponent {
-  public displayedColumns: string[] = ['id', 'name', 'iso2', 'iso3'];
+  public displayedColumns: string[] = ['id', 'name', 'iso2', 'iso3','totalCities'];
   public countries: MatTableDataSource<Country>;
   defaultPageIndex: number = 0;
   defaultPageSize: number = 10;
